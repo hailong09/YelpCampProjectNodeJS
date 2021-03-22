b99a42a5f0948aa39530b7c685fb55b128947c1e
unknown <long2qw@gmail.com>
2021-03-22 18:28:22 -0400

    add error handle for all the app

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
-const campgroundSchema = require('./schemas')
+const {campgroundSchema} = require('./schemas');
+const {reviewSchema} = require('./schemas');
+const Review = require('./models/review');
 
 //connect to mongodb
 mongoose.connect('mongodb://localhost:27017/yelpCamp', {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true});
@@ -34,9 +36,19 @@ app.use(express.json());
 //methodoveride middleware
 app.use(methodOverride("_method"));
 
-
+//join validate function
 const validateCampground = (req, res, next) => {
     const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
+const validateReview = (req, res, next) => {
+    const {error} = reviewSchema.validate(req.body);
     if (error) {
         const msg = error.details.map(el => el.message).join(',')
         throw new ExpressError(msg, 400)
@@ -92,6 +104,16 @@ app.delete('/campgrounds/:id', catchAsync( async (req, res) =>{
     const {id} = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds')
+}))
+
+app.post('/campgrounds/:id/reviews',reviewSchema,catchAsync(async (req,res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+
 }))
 
 app.all('*', (req, res, next) => {
--- schemas.js
+++ schemas.js
@@ -5,9 +5,17 @@
         title: Joi.string().required(),
         price: Joi.number().required().min(0),
         image: Joi.string().required(),
-        location: Joi.string().required(),
+        location: Joi.string().min(1).max(5).required(),
         description: Joi.string().required(),
     }).required()
 })
 
-module.exports = campgroundSchema
+const reviewSchema = Joi.object({
+    review: Joi.object({
+        rating: Joi.number().required(),
+        body: Joi.string().required(),
+    }).required()
+})
+
+module.exports.campgroundSchema = campgroundSchema
+module.exports.reviewSchema = reviewSchema
