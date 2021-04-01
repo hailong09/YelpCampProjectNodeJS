6977662bb92b303c2d8e68b128168220907cc84d
unknown <long2qw@gmail.com>
2021-03-31 20:03:48 -0400

    added flash to the app

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-// const session = require('express-session');
-// const flash = require('connect-flash');
+const session = require('express-session');
+const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const campgrounds = require('./routes/campgrounds');
@@ -30,24 +30,36 @@ app.engine("ejs", ejsMate)
 //Allow shoing req.body
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-
 //methodoveride middleware
 app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
 
-//join validate function
-// app.use((req, res, next) => {
-//     res.locals.success = req.flash('success');
-//     res.locals.error = req.flash('error');
-//     next();
-// })
-app.get('/', (req, res) => {
-    res.render('home')
-});
+const sessionConfig = {
+    secret: 'thisshoudbeabettersecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true, 
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge:1000 * 60 * 60 * 24 * 7, 
+    }
+}
+
+app.use(session(sessionConfig))
+app.use(flash());
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
 
 app.use('/campgrounds', campgrounds)
 app.use('/campgrounds/:id/reviews', reviews)
 
-
+app.get('/', (req, res) => {
+    res.render('home')
+});
 
 
 app.all('*', (req, res, next) => {
