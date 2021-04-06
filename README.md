fd443a4d17855cd1458e007f46af736c6c0f6eab
unknown <long2qw@gmail.com>
2021-04-06 19:41:31 -0400

    refactor by adding controller dir for the app and ajusted UI rating

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # YelpCampProjectNodeJS
 This is the server-side web app the provide the best camp informations
+
--- routes/users.js
+++ routes/users.js
@@ -1,54 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
-const ExpressError = require('../utils/ExpressError');
 const catchAsync = require('../utils/catchAsync');
-const { route } = require('./campgrounds');
 const passport = require('passport');
+const users = require('../controllers/users');
+
 
 // app.get('/fakeUser', async (req,res) => {
 //     const user = new User({email: 'longg@gmail.com', username:'long'})
 //     const newUser = await User.register(user, '123456');
 //     res.send(newUser);
 // })
-router.get('/register', (req, res) => {
-    res.render('users/register')
-})
 
-router.post('/register', catchAsync(async (req,res) => {
-    try{
-        const {username, password, email} = req.body;
-        const user =  new User({email, username});
-        const registeredUser = await User.register(user, password);
-        req.login(registeredUser, err => {
-            if(err){return next(err);}
-            req.flash('success' , 'Welcome to Yelp Camp!');
-            res.redirect('/campgrounds') 
-        })
-        
-    }catch(e){
-        req.flash('error', e.message);
-         res.redirect('/register')
-    }
-    
-}))
+router.route('/register')
+    .get(users.renderRegister)
+    .post( catchAsync(users.register))
 
-router.get('/login', (req, res) => {
-    res.render('users/login');
-})
+router.route('/login')
+    .get( users.renderLogin )
+    .post(passport.authenticate('local', {failureRedirect: '/login',failureFlash: true }), users.login )
 
-router.post('/login',  passport.authenticate('local', {
-    failureRedirect: '/login',failureFlash: true }), (req, res) => {
-        req.flash('success' , 'Welcome back!');
-        const redirectUrl = req.session.returnTo || '/campgrounds'
-        delete req.session.returnTo
-        res.redirect(redirectUrl);
+router.get('/logout', users.logout);
 
-})
-
-router.get('/logout',(req, res) => {
-    req.logout();
-    req.flash('success', "Loged You Out!")
-    res.redirect('/campgrounds');
-  });
 module.exports = router
